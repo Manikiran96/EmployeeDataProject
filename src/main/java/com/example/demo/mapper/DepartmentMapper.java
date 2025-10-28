fce42e0074f3c40f4afb97951cbf2107c1896742
Manikiran096 <batarajumanikiran950@gmail.com>
2025-10-28 11:51:53 +0530

    oneprojectcommit

---
+++ src/main/java/com/example/demo/mapper/DepartmentMapper.java
@@ -0,0 +1,28 @@
+package com.example.demo.mapper;
+
+
+
+
+import com.example.demo.dto.DepartmentDto;
+import com.example.demo.entity.Department;
+
+public class DepartmentMapper {
+
+    // convert department jpa entity into department dto
+    public static DepartmentDto mapToDepartmentDto(Department department){
+        return new DepartmentDto(
+                department.getId(),
+                department.getDepartmentName(),
+                department.getDepartmentDescription()
+        );
+    }
+
+    // convert department dto into department jpa entity
+    public static Department mapToDepartment(DepartmentDto departmentDto){
+        return new Department(
+                departmentDto.getId(),
+                departmentDto.getDepartmentName(),
+                departmentDto.getDepartmentDescription()
+        );
+    }
+}
---
+++ src/main/java/com/example/demo/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,70 @@
+package com.example.demo.service.impl;
+
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.dto.DepartmentDto;
+import com.example.demo.entity.Department;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.mapper.DepartmentMapper;
+import com.example.demo.repository.DepartmentRepository;
+import com.example.demo.service.DepartmentService;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class DepartmentServiceImpl implements DepartmentService {
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Override
+    public DepartmentDto createDepartment(DepartmentDto departmentDto) {
+        Department department = DepartmentMapper.mapToDepartment(departmentDto);
+        Department savedDepartment = departmentRepository.save(department);
+        return DepartmentMapper.mapToDepartmentDto(savedDepartment);
+    }
+
+    @Override
+    public DepartmentDto getDepartmentById(Long departmentId) {
+        Department department = departmentRepository.findById(departmentId).orElseThrow(
+                () -> new ResourceNotFoundException("Department is not exists with a given id: " + departmentId)
+        );
+        return DepartmentMapper.mapToDepartmentDto(department);
+    }
+
+    @Override
+    public List<DepartmentDto> getAllDepartments() {
+        List<Department> departments = departmentRepository.findAll();
+        return departments.stream().map((department) -> DepartmentMapper.mapToDepartmentDto(department))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public DepartmentDto updateDepartment(Long departmentId, DepartmentDto updatedDepartment) {
+
+        Department department = departmentRepository.findById(departmentId).orElseThrow(
+                () -> new ResourceNotFoundException("Department is not exists with a given id:"+ departmentId)
+        );
+
+        department.setDepartmentName(updatedDepartment.getDepartmentName());
+        department.setDepartmentDescription(updatedDepartment.getDepartmentDescription());
+
+        Department savedDepartment = departmentRepository.save(department);
+
+        return DepartmentMapper.mapToDepartmentDto(savedDepartment);
+    }
+
+    @Override
+    public void deleteDepartment(Long departmentId) {
+        departmentRepository.findById(departmentId).orElseThrow(
+                () -> new ResourceNotFoundException("Department is not exists with a given id: " + departmentId)
+        );
+
+        departmentRepository.deleteById(departmentId);
+    }
+}
